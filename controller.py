0000d12908205d0a57c17e3f517eaedebab4b758
The6apcuk <b_naboka@rocketmail.com>
2019-02-04 22:57:14 +0200

    returned eps, started integrations with model

--- controller.py
+++ controller.py
@@ -1,19 +1,24 @@
 from view import Ui_MainWindow
-from model import Requests
+from model import Requests, Endpoints
 from persistent_model import FileInt
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 
+class Colors:
+    red = (255, 0, 0)
+    white = (255, 255, 255)
 
 class Controller:
-    def __init__(self, requests ):
+    def __init__(self, requests, eps):
         self.app = QtWidgets.QApplication([])
         self.main_window = QtWidgets.QMainWindow()
         self.window = Ui_MainWindow()
 
         self.requests = requests
+        self.eps = eps
 
         self.window.setupUi(self.main_window)
+        self.config_tables = self.window.tableWidget_header_config, self.window.tableWidget_body_config
         self.configure_callbacks(self.window)
         self.start_app()
 
@@ -45,12 +50,26 @@ class Controller:
         name_item = self.window.tableWidget_request.item(row, 0)
         if hasattr(name_item, 'text'):
             # try to get information from dbs and fill all tables in config window
-            self.window.lineEdit_request_name.setText(name_item.text())
+            self.window.lineEdit_request_name_config.setText(name_item.text())
         self.window.stackedWidget.setCurrentWidget(self.window.config)
 
     def button_add_request_action(self,*x):
         #self.requests
         self.window.stackedWidget.setCurrentWidget(self.window.config)
+
+    def clear_config_tables_content(self, *tables):
+        for table in tables:
+            rows = table.rowCount()
+            columns = table.columnCount()
+            for column in range(columns):
+                for row in range(rows):
+                    item = table.item(row, column)
+                    if item:
+                        table.setItem(row, column, QtWidgets.QTableWidgetItem())
+                        # table.item(row, column).setBackground(QtGui.QColor(*Colors.white))
+
+        self.window.stackedWidget.setCurrentWidget(self.window.request)
+
 
     def test_filled_tables(self, *tables):
         for table in tables:
@@ -60,24 +79,32 @@ class Controller:
             for column in range(columns):
                 for row in range(rows):
                     item = table.item(row, column)
-                    if not hasattr(item,'text') or item.background().color().getRgb() == (100,100,150,255) or not item.text():
+                    if not (item and item.text()) or item.background().color().getRgb() == Colors.red:
                         table.setItem(row, column, QtWidgets.QTableWidgetItem())
-                        table.item(row, column).setBackground(QtGui.QColor(100, 100, 150))
+                        table.item(row, column).setBackground(QtGui.QColor(*Colors.red))
                         failed_items += (row, column)
 
         return False if failed_items else True
 
     def button_apply_config_action(self, *x):
-        result = self.test_filled_tables(self.window.tableWidget_header_config, self.window.tableWidget_body_config)
+        result = self.test_filled_tables(*self.config_tables)
 
         # self.add_row(self.window.tableWidget_request, self.requests)
         #get info
         if result:
+            self.clear_config_tables_content(*self.config_tables)
             self.window.stackedWidget.setCurrentWidget(self.window.request)
 
-    def table_update(self, table, item, update=True):
-        if item.text():
-            item.setBackground(QtGui.QColor(255, 255, 255))
+    def table_update(self, item, model):
+        column = item.column()
+        row = item.row()
+        data = item.data(0)
+        model.update(row, column, data)
+
+        if hasattr(item, 'background'):
+            item.setBackground(QtGui.QColor(*Colors.white))
+
+
 
 
     def configure_callbacks(self, window):
@@ -90,16 +117,24 @@ class Controller:
         window.actionEdit_URI.triggered.connect(lambda *x: window.stackedWidget.setCurrentWidget(window.uri))
 
         # config window buttons
-        window.button_body_add.clicked.connect(lambda *x: self.add_row(window.tableWidget_body_config))
-        window.button_body_delete.clicked.connect(lambda *x: self.del_row(window.tableWidget_body_config))
+        window.button_body_add_config.clicked.connect(lambda *x: self.add_row(window.tableWidget_body_config, 'requests'))
+        window.button_body_delete_config.clicked.connect(lambda *x: self.del_row(window.tableWidget_body_config, 'requests'))
 
-        window.button_header_add.clicked.connect(lambda *x: self.add_row(window.tableWidget_header_config))
-        window.button_header_delete.clicked.connect(lambda *x: self.del_row(window.tableWidget_header_config))
+        window.button_header_add_config.clicked.connect(lambda *x: self.add_row(window.tableWidget_header_config, 'requests'))
+        window.button_header_delete_config.clicked.connect(lambda *x: self.del_row(window.tableWidget_header_config, 'requests'))
 
         window.button_apply_config.clicked.connect(self.button_apply_config_action)
+        window.button_cancel_config.clicked.connect(lambda *x: self.clear_config_tables_content(*self.config_tables))
 
         window.tableWidget_header_config.itemChanged.connect(lambda item: self.table_update(window.tableWidget_header_config, item))
         window.tableWidget_body_config.itemChanged.connect(lambda item: self.table_update(window.tableWidget_body_config,item))
+
+        # uri window
+        window.button_add_uri.clicked.connect(lambda *x: self.add_row(window.tableWidget_uri, eps))
+        window.button_delete_uri.clicked.connect(lambda *x: self.del_row(window.tableWidget_uri, 'ep'))
+        window.button_apply_uri.clicked.connect(lambda *x: window.stackedWidget.setCurrentWidget(window.request))
+
+        window.tableWidget_uri.itemChanged.connect(lambda item: self.table_update(item, self.eps))
 
     def start_app(self):
         self.main_window.show()
@@ -107,4 +142,5 @@ class Controller:
 
 if __name__ == "__main__":
     requests = Requests(FileInt('URI_DBS', str(Requests.name)))
-    Controller(requests)
+    eps = Endpoints(FileInt('URI_DBS', str(Endpoints.name)))
+    Controller(requests, eps)
--- model.py
+++ model.py
@@ -1,18 +1,13 @@
+from PyQt5 import QtCore
 
-
-class Request:
-    def __init__(self, name, headers, bodies):
-        self.name = name
-        self.headers = headers[self.name]
-        self.bodies = bodies[self.name]
-
-
-class Requests:
-    name = 'Requests'
+class CollectionModel(QtCore.QObject):
     def __init__(self, dtb):
+        super().__init__()
+        self.name = self.__class__.__name__
         self.dtb = dtb
-        self.collection = {}
+        self.collection = []
         self.get_data()
+        print(ItemModel)
 
     def __len__(self):
         return len(self.collection)
@@ -23,25 +18,86 @@ class Requests:
     def __str__(self):
         return self.__class__.__name__
 
+    def add(self, request):
+        self.collection += [request]
+        self.dtb.write(self.collection, self.name)
+        print(self.collection)
+
     def get_data(self):
         for request in self.dtb.read(self.name):
             self.add(request)
 
-    def add(self, request):
-        self.collection[request.name] = request
-        self.dtb.write(self.collection, self.name)
-        print(self.collection)
-
-    def delete(self, name):
-        if not name:
+    def delete(self, num):
+        if num == -1:
             print('doing nothing')
             return
-        self.collection.pop(name)
+        self.collection.pop(num)
         self.dtb.write(self.collection, self.name)
 
     def update(self, row, column, value):
+        self.collection[row].update(column, value)
+        self.dtb.update(self.name, row, self.collection[row])
 
-        #self.collection[row].update(column, value)
-        #self.dtb.update(self.name, row, self.collection[row])
-        pass
+
+class ItemModel:
+    def __repr__(self):
+        return '{} {}'.format(self.__class__.__name__, self.__dict__)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def update(self, column, value):
+        key = self.index_to_key_map[column]
+        self.__setattr__(key, value)
+        print('Updated key {} -> {}'.format(key, repr(self)))
+
+        # for key, value in values.items():
+        #     if self.__dict__.get(key):
+        #         self.__setattr__(key, value)
+        #         continue
+        #     raise AttributeError('"{}" class has no attribute "{}"'.format(self.__class__.__name__, key))
+
+
+    def __getitem__(self, item):
+        key = self.index_to_key_map[item]
+
+        """Returns value from key/values of instance attributes"""
+        return self.get_attributes()[key]
+
+    def get_attributes(self):
+        """Gets attributes, assuming that attributes are strings"""
+        return {attr_name: attr_value for attr_name, attr_value in self.__dict__.items() if type(attr_value) in (str,int)}
+
+
+class Endpoint(ItemModel):
+    index_to_key_map = {0: 'endpoint', 1: 'header_values', 2: 'body_values'}
+
+    def __init__(self, uri, header_names, body_names):
+        self.uri = uri
+        self.header_names = header_names
+        self.body_names = body_names
+
+class Request(ItemModel):
+    index_to_key_map = {0: 'uri', 1: 'header_names', 2: 'body_names'}
+
+    def __init__(self, name, headers, bodies):
+        self.name = name
+        self.headers = headers[self.name]
+        self.bodies = bodies[self.name]
+
+
+
+class Endpoints(CollectionModel):
+    item = Endpoint
+    name = 'Endpoints'
+
+    def __init__(self, dtb):
+        CollectionModel.__init__(self, dtb)
+
+
+class Requests(CollectionModel):
+    name = 'Requests'
+    def __init__(self, dtb):
+        CollectionModel.__init__(self, dtb)
+
 
